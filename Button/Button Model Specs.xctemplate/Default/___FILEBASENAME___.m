86df630afb049792b23316409b06214155d220dc
Wes Smith <wes@w5mith.com>
2014-10-03 12:31:36 -0400

    fixes spec template issues

--- Button/Button Model Specs.xctemplate/Default/___FILEBASENAME___.m
+++ Button/Button Model Specs.xctemplate/Default/___FILEBASENAME___.m
@@ -1,7 +1,7 @@
 #import "Specta.h"
-#import "___FILEBASENAME___.h"
+___IMPORTHEADER_subjectClassName___
 
-SpecBegin(___FILEBASENAMEASIDENTIFIER___)
+SpecBegin(___VARIABLE_subjectClassName:identifier___)
 
 #warning create your object representations
 NSDictionary *representation  = @{ <#key#>: <#value#> };
@@ -13,22 +13,22 @@ NSDictionary *representation2 = @{ <#key#>: <#differentValue#> };
 describe(@"Initialization", ^{
     
     it(@"should create an instance from a valid dictionary", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
 #warning compare object's properties are equal to corresponding representation values
     });
     
     it(@"should not create an instance from an invalid dictionary", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:@{ @"invalidKey": @1234 }];
+        ___VARIABLE_subjectClassName:identifier___ *object = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:@{ @"invalidKey": @1234 }];
         expect(object).to.beNil();
     });
     
     it(@"should not create an instance from an empty dictionary", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:@{}];
+        ___VARIABLE_subjectClassName:identifier___ *object = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:@{}];
         expect(object).to.beNil();
     });
     
     it(@"should not create an instance from an nil dictionary", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:nil];
+        ___VARIABLE_subjectClassName:identifier___ *object = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:nil];
         expect(object).to.beNil();
     });
     
@@ -40,38 +40,38 @@ describe(@"Initialization", ^{
 
 describe(@"Equality", ^{
     
-    it(@"should consider two identical ___FILEBASENAMEASIDENTIFIER___ instances equal", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object1 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
-        ___FILEBASENAMEASIDENTIFIER___ *object2 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
+    it(@"should consider two identical ___VARIABLE_subjectClassName:identifier___ instances equal", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object1 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object2 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
         expect([object1 isEqual:object2]).to.beTruthy();
     });
     
-    it(@"should consider two identical ___FILEBASENAMEASIDENTIFIER___ instances hash values to be equal", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object1 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
-        ___FILEBASENAMEASIDENTIFIER___ *object2 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
+    it(@"should consider two identical ___VARIABLE_subjectClassName:identifier___ instances hash values to be equal", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object1 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object2 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
         expect([object1 hash]).to.equal([object2 hash]);
     });
     
-    it(@"should consider two different ___FILEBASENAMEASIDENTIFIER___ instances to be inequal", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object1 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
-        ___FILEBASENAMEASIDENTIFIER___ *object2 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation2];
+    it(@"should consider two different ___VARIABLE_subjectClassName:identifier___ instances to be inequal", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object1 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object2 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation2];
         expect([object1 isEqual:object2]).to.beFalsy();
     });
     
-    it(@"should consider nil to be different to a valid ___FILEBASENAMEASIDENTIFIER___ instance", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
+    it(@"should consider nil to be different to a valid ___VARIABLE_subjectClassName:identifier___ instance", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
         expect([object isEqual:nil]).to.beFalsy();
     });
     
-    it(@"should consider an empty ___FILEBASENAMEASIDENTIFIER___ instance to be different to a valid ___FILEBASENAMEASIDENTIFIER___ instance", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object1 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
-        ___FILEBASENAMEASIDENTIFIER___ *object2 = [[___FILEBASENAMEASIDENTIFIER___ alloc] init];
+    it(@"should consider an empty ___VARIABLE_subjectClassName:identifier___ instance to be different to a valid ___VARIABLE_subjectClassName:identifier___ instance", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object1 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object2 = [[___VARIABLE_subjectClassName:identifier___ alloc] init];
         expect([object1 isEqual:object2]).to.beFalsy();
     });
     
-    it(@"should consider two empty ___FILEBASENAMEASIDENTIFIER___ instances to be equal", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object1 = [[___FILEBASENAMEASIDENTIFIER___ alloc] init];
-        ___FILEBASENAMEASIDENTIFIER___ *object2 = [[___FILEBASENAMEASIDENTIFIER___ alloc] init];
+    it(@"should consider two empty ___VARIABLE_subjectClassName:identifier___ instances to be equal", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object1 = [[___VARIABLE_subjectClassName:identifier___ alloc] init];
+        ___VARIABLE_subjectClassName:identifier___ *object2 = [[___VARIABLE_subjectClassName:identifier___ alloc] init];
         expect([object1 isEqual:object2]).to.beTruthy();
     });
 });
@@ -80,7 +80,7 @@ describe(@"Equality", ^{
 describe(@"Serialization", ^{
     
     it(@"Serialization and deserialization should be complete & symmetrical", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
         NSDictionary *dictionary = [object dictionaryRepresentation];
         expect(dictionary).to.equal(representation);
     });
@@ -89,9 +89,9 @@ describe(@"Serialization", ^{
 
 describe(@"Copying", ^{
     
-    it(@"should create a valid copy of a ___FILEBASENAMEASIDENTIFIER___ instance", ^{
-        ___FILEBASENAMEASIDENTIFIER___ *object1 = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithDictionary:representation];
-        ___FILEBASENAMEASIDENTIFIER___ *object2 = [object1 copy];
+    it(@"should create a valid copy of a ___VARIABLE_subjectClassName:identifier___ instance", ^{
+        ___VARIABLE_subjectClassName:identifier___ *object1 = [[___VARIABLE_subjectClassName:identifier___ alloc] initWithDictionary:representation];
+        ___VARIABLE_subjectClassName:identifier___ *object2 = [object1 copy];
         expect([object1 isEqual:object2]).to.beTruthy();
     });
 });
